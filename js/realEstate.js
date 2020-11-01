bfa26a34f4626857ffd1ae39de0d9fe53f1c9f35
Austin Hartley <50050814+aahartley@users.noreply.github.com>
2020-11-01 15:33:25 -0500

    update

--- js/realEstate.js
+++ js/realEstate.js
@@ -1,5 +1,9 @@
 document.getElementById('bt5').addEventListener('click',display);
 document.getElementById('bt').addEventListener('click',calculate);
+document.getElementById('bt3').addEventListener('click',home);
+function home(){
+    window.location=("index.html");
+}
 function display(){
     var a = document.getElementById("form").style.visibility="visible";
     var b = document.getElementById("test").style.visibility="hidden";
@@ -12,36 +16,28 @@ function calculate(){
     var iRate = parseFloat(document.getElementById('input2').value);
     iRate = parseFloat(iRate / 100);
     var lTerm = parseInt(document.getElementById('input3').value);
-    var date = document.getElementById('year').value;
+    var date = parseInt(document.getElementById('year').value);
     var month = document.getElementById('date').selectedIndex;
-    var show = document.getElementById('results').value;
     var balance = loanAmount;
 
     var monthlyInterestRate = parseFloat(iRate/12);
-
-    console.log("test"+monthlyInterestRate);
-
-    var test4 = loanAmount*((monthlyInterestRate*(Math.pow((1.00+monthlyInterestRate),lTerm*12))));
-    var test1= Math.pow((1.00+monthlyInterestRate),(lTerm*12))-1;
-    var monthlyPayment = test4/test1;
+    var mp1 = loanAmount*((monthlyInterestRate*(Math.pow((1.00+monthlyInterestRate),lTerm*12))));
+    var mp2= Math.pow((1.00+monthlyInterestRate),(lTerm*12))-1;
+    var monthlyPayment = mp1/mp2;
     var mp = monthlyPayment.toFixed(2);
 
 
     var interestPaidPerMonth = balance * monthlyInterestRate;
     var principalPaidPerMOnth = monthlyPayment-interestPaidPerMonth;
     
-
-
- 
     var interestpayment =0;
     var principalpayment =0;
-    console.log(mp);
 
-    var result="<p id=\"mp\">Monthy payment is: "+mp+"</p><table>";
+    var result="<p id=\"mp\">Monthy Payment is: "+mp+"</p><table>";
 
     for(var i=1; i<=lTerm; i++){
         if(i==1)
-        result += "<tr>"+"<th>date </th>"+" <th>interest </th>"+"<th>principal </th>"+" <th>balance </th>"+"</tr>";
+        result += "<tr>"+"<th>Date </th>"+" <th>Interest </th>"+"<th>Principal </th>"+" <th>Balance </th>"+"</tr>";
 
         result += "<tr>";
 
@@ -56,7 +52,7 @@ function calculate(){
             var bb = balance.toFixed(2);
 
             interestpayment += interestPaidPerMonth;
-            var nn = interestpayment.toFixed(2);
+            var ii = interestpayment.toFixed(2);
 
             principalpayment += monthlyPayment-interestPaidPerMonth;
             var pp = principalpayment.toFixed(2);
@@ -64,15 +60,11 @@ function calculate(){
 
         }
 
-        result += "<td>"+(month+1)+"/"+(date)+"-"+(month+1)+"/"+(date)+" </td> <td>"+bb+"</td><td> "+nn+" </td><td>"+pp+"</td>";
+        result += "<td>"+(month+1)+"/"+(date)+"-"+(month+1)+"/"+(date+1)+" </td> <td>$"+ii+"</td><td> $"+pp+" </td><td>$"+bb+"</td>";
         date++;
 
         var test = document.getElementById('test');
         test.innerHTML = result;
-        console.log("year" +i);
-        console.log(bb);
-        console.log(pp);
-        console.log(nn);
         interestpayment=0;
         principalpayment=0;
         result += "</tr>"
@@ -81,9 +73,4 @@ function calculate(){
     result = "</table>";
 
 
-
-
-
- 
-  
 }
